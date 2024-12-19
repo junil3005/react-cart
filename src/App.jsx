2c73135acd7611d99f88313103b5d38d81b305c9
junil3005 <yjl3005@naver.com>
2024-12-19 18:05:27 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import CartHeader from "./components/CartHeader";
 import ShopList from "./components/ShopList";
@@ -7,14 +7,21 @@ import BoughtList from "./components/BoughtList";
 import CartFooter from "./components/CartFooter";
 
 function App() {
-  const [itemList, setItemList] = useState([
-    { id: 1, name: "무", isBought: false },
-    { id: 2, name: "배추", isBought: false },
-    { id: 3, name: "쪽파", isBought: true },
-    { id: 4, name: "고춧가루", isBought: false },
-  ]);
+  const apiUrl = "http://localhost:3000/shoplist";
+//  서버로부터 API 호출해서 쇼핑 목록 받아오기
+//  const [itemList, setItemList] = useState([ 
+  //   { id: 1, name: "무", isBought: false },
+  //   { id: 2, name: "배추", isBought: false },
+  //   { id: 3, name: "쪽파", isBought: true },
+  //   { id: 4, name: "고춧가루", isBought: false },
+  // ]);
+  const [itemList, setItemList] = useState([]);
   //  산 물건 보기 여부를 체크할 state
   const [showBoughtItems, setShowBoughtItems] = useState(true);
+  //  페이지 로딩 상태 체크 state
+  const [isLoading, setIsLoading] = useState(true);
+  // 에러 메시지 출력을 위한 state
+  const [error, setError] = useState(null);
 
   //  isBought === false인 것만 필터링
   //  isBought === false인 것들
@@ -22,6 +29,29 @@ function App() {
   //  isBought === true인 것들의 목록
   const boughtItems = itemList.filter((item) => item.isBought);
 
+  //  API에서 목록 받아오는 함수
+  const fetchItems = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("데이터를 받아오지 못했습니다."); 
+      }
+      const data = await response.json();
+      // console.log(data);
+      setItemList(data);
+      setIsLoading(false);  //  로딩이 끝났음을 알림
+    } catch (err) {
+      // console.error(err);
+      setError(err.message);
+      setIsLoading(false);  //  로딩이 끝남
+    } 
+  };
+  useEffect(() => {
+  fetchItems();
+  }, []); //  -> 컴포넌트가 처음 로딩되었을 때의 이펙트 발생
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>에러: {error}</div>;
   //  새 아이템 추가
   const addNewItem = (name) => {
     //  id 생성 -> id의 최댓값 + 1
